084f0c0cb6e12ced3031f002de6269209da5537b
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-11 20:24:13 +0000

    'new'

--- push.py
+++ push.py
@@ -34,5 +34,6 @@ for word in words:
               print(word)
             # Program to sort alphabetically the words form a string provided by the user
             
+            my_str = "Hello this Is an Example With cased letters"
             
               
