ea064e10cb9156f9b706862e1dbbabb894dfaf11
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-11 20:25:54 +0000

    'new'

--- push.py
+++ push.py
@@ -46,5 +46,6 @@ for word in words:
             # display the sorted words
             
             print("The sorted words are:")
-            
+            for word in words:
+                
               
