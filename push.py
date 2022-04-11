377e72ce1b7256f3d5102a6cc4688c013525a2b6
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-11 20:32:04 +0000

    'new'

--- push.py
+++ push.py
@@ -90,5 +90,6 @@ for word in words:
                                  
                                  
                                  # breakdown the string into a list of words
+                                 words = my_str.split()
                                  
               
