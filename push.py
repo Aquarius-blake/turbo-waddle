de7065e24cf6c0156d269fc1a8ea0e3b17a67d0b
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-11 20:27:44 +0000

    'new'

--- push.py
+++ push.py
@@ -59,5 +59,6 @@ for word in words:
                    words = my_str.split()
                    
                    # sort the list
+                   words.sort()
                    
               
