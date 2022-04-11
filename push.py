038195fce3ca079b322df0c3f0d19347e4c15a9a
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-11 20:30:56 +0000

    'new'

--- push.py
+++ push.py
@@ -82,5 +82,6 @@ for word in words:
                           
                           print("The sorted words are:")
                           for word in words:
-                              
+                                 print(word)
+                                 
               
